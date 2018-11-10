df491a0000e80b9837e237c657880a124147254d
designerzen <designerzen@gmail.com>
2018-11-10 16:29:38 +0000

    Read file variants

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+
+
+import readFile from './read-file'
+
+const textfile = "./tc-stripped.txt"
+
+//import text from "./tx-stripped.txt"
+console.log("Reading Ts and Cs")
+
+readFile(textfile).next(
+    text => {
+    console.log(text)
+    }
+)
---
+++ read-file.js
@@ -0,0 +1,25 @@
+// import read from 'read-file'
+
+
+// export const readFile = () => {
+//     read.sync('tc.txt', 'utf8', (err, string) => {
+//         console.log(string.split(' ').length)
+//     })
+// }
+
+const readFile = (textFile)=>{
+
+    return new Promise((resolve, reject) => {
+        const client = new XMLHttpRequest()
+        client.open("GET", textFile)
+        client.onreadystatechange = () => {
+            resolve(client.responseText)
+        }
+        client.onerror = (error)=>{
+            reject(error)
+        }
+        client.send()
+    })
+}
+
+export default readFile
