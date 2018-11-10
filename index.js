8c67b81999c8814cbe8e9b946d8353cdd8b0fcbb
designerzen <designerzen@gmail.com>
2018-11-10 22:18:41 +0000

    Index elements from DOM

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 //const string = fs.readFileSync(__dirname + "/test.txt", "utf8");
 //import text from './assets/tc-stripped.txt'
 import textFile from "./assets/tc-stripped.txt"
+import snareFile from "./assets/snare.wav"
+import kickFile from "./assets/kick.wav"
 
 import {readFile} from './read-file'
 import {analyse} from './sentiment-analyser'
 
 // import {convertTextToSound } from './audio'
 import {
+    SYNTH_AM, SYNTH_FM, SYNTH_MONO, SYNTH_GENERIC,
   generateSentimentSong,
   fetchScales,
   startAudio,
   createClip,
   createLead,
-  createPercussion,
+    createKicks,
+    createSnares,
   changeRow
 } from "./audio"
 
@@ -30,10 +34,12 @@ let words = []
 let word = 0
 
 let lead
-let drums
+let kicks
+let snares
 
 const elements = {
-    ticker: document.getElementById('ticker')
+    ticker: document.getElementById('ticker'),
+    data: document.getElementById('data')
 }
 
 const run = () =>{
@@ -64,29 +70,54 @@ const run = () =>{
             return fetchScales()
         }
 
-    ).then(scales => {
+    ).then( allScales => {
 
         progress = 3 / STEPS
-        
 
-        // const clip = createClip()
-        // clip.start()
+        lead = createLead(
+            
+            [
 
-        lead = createLead()
-        drums = createPercussion()
+                // SAD
+                {
+                    pattern: "xxxxx",
+                    notes: 'C4 D#4'
+                },
+                {
+                    pattern: "xxx[xx]",
+                    notes: 'C2 D#4'
+                },
+                {
+                    pattern: "x-x-x-x",
+                    notes: 'C3 D#4'
+                },
+                // HAPPY
+                {
+                    pattern: "xxx[-x]",
+                    notes: 'C1 D#4'
+                }
+            ]
+        )
+        kicks = createKicks()
+        snares = createSnares()
 
-        console.log("Audio READY!", {scales, lead, drums});
+        console.log("Audio READY!", { allScales, lead, kicks});
 
         // now trigger certain parts...
         //lead.startRow(1)
-//console.log(lead)
-
+        //console.log(lead)
 
         startAudio(1, BPM, time=>{
             const tock = bar++%2 === 0
             const nextWord = words[word++]
 
+            // to change sentiment...
+            //changeRow(test++)
+
             console.log("Beat", time, nextWord)
+            // update the sentiment obect...
+            elements.data.innerHTML = nextWord
+
             elements.ticker.innerHTML = nextWord
             elements.ticker.className = "beat-" + bar + " " + (tock? 'tock' : 'tick')
         })
@@ -102,7 +133,7 @@ const run = () =>{
             //     console.error('fucked')
             // }
             
-            changeRow(test++)
+            
             
         })
 
