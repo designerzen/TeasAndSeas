c6fd9893ae6d36ebe3b07eaf48f6f884c0ecc0f0
Luis <luiszayas@posteo.net>
2018-11-10 16:21:53 +0000

    Sentiment test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('hello')
+	console.log('hello')
+
+	var Sentiment = require('sentiment');
+	var sentiment = new Sentiment();
+	var result = sentiment.analyze('Cats are stupid.');
+console.dir(result);    // Score: -2, Comparative: -0.666
