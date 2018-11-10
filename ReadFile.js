92cd0196660ca94ef5b5f147d22e4f9a4c40e9c3
Johnnie Hård <johnnie@synvinkel.org>
2018-11-10 16:21:26 +0100

    parcel boiler

---
+++ ReadFile.js
@@ -0,0 +1,10 @@
+// const read = require('read-file')
+// const textfile = 'tx-stripped.txt'
+
+// export const readFile = () => {
+//     read.sync('tc.txt', 'utf8', (err, string) => {
+//         console.log(string.split(' ').length)
+//     })
+// }
+
+
