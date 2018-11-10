03bcd0b27a4b4ac536e4bb6c11b897d7bb8f361b
designerzen <designerzen@gmail.com>
2018-11-10 18:02:26 +0000

    Audio lib and chained promises for index

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-
+// Parcel requires absolute imports for lazy loaded files
+//const string = fs.readFileSync(__dirname + "/test.txt", "utf8");
+//import text from './assets/tc-stripped.txt'
+import text from "./assets/tc-stripped.txt"
 
 import {readFile} from './read-file'
 import {analyse} from './sentiment-analyser'
 
-//import text from "./assets/tc-stripped.js"
+// import {convertTextToSound } from './audio'
+import { generateSentimentSong } from "./audio"
+//const file = __dirname + text
+const file = text
+console.log("Reading Ts and Cs from", file )
 
-const textfile = "./assets/tc-stripped.js"
-
-//import text from "./tx-stripped.txt"
-console.log("Reading Ts and Cs")
-console.log("readFile", readFile)
-console.log("analyse", analyse)
-
-readFile(textfile).then(
+readFile(file).then(
     text => {
         console.log(text)
         return analyse(text)
     }
 ).then(
+
     analysedText=>{
 
+        // first split text by sentence...
+        const sentences = text.split('.')
+        const words = text.split(" ")
+
+        console.table(analysedText)
+
+        console.error("Text > ", sentences, words)
+
+        return generateSentimentSong(0)
     }
-)
 
-analyse().then( results =>{
-    console.table(results)
+).catch( error =>{
+
+    console.error(error)
 })
-
-// console.dir(result)    // Score: -2, Comparative: -0.666
