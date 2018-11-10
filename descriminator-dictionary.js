1b5f7183a5b7211eb6becb05e6ed567d981c404d
designerzen <designerzen@gmail.com>
2018-11-10 15:52:05 +0000

    descrimitor

---
+++ descriminator-dictionary.js
@@ -0,0 +1,23 @@
+// The list of "SMALL words" which are not capped comes from
+// the New York Times Manual of Style, plus 'vs' and 'v'.
+export const dictionary = [
+    "a",
+    "an",
+    "and",
+    "as",
+    "at",
+    "but",
+    "by",
+    "en",
+    "for",
+    "if",
+    "in",
+    "of",
+    "on",
+    "or",
+    "the",
+    "to",
+    "v\.?",
+    "via",
+    "vs"
+] 
--- descriminator.js
+++ descriminator.js
@@ -1,7 +1,15 @@
-const descriminate (text) => {
+import { dictionary } from 'descriminator-dictionary'
+
+const descriminate = (text) => {
 
     // This is resposnible for loading in a file and removing 
-    // certain 
+    // certain words listed bove in the dictionary file
+    // remove all words from the strings using a loop
+    dictionary.forEach(phrase => {
+        text.replace(phrase,"")
+    })
+
+    return text
 }
 
 export default descriminate
